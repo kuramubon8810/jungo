b7ce1422ebf8ab6f7702bcfc3a6aa4effbcc36ae
kuramubon8810 <nasa8810@gmail.com>
2019-01-23 15:36:22 +0900

    とりあえずの完成版

--- jyungo.js
+++ jyungo.js
@@ -63,6 +63,20 @@ let clicked_board = (e) => {
     };
 
     if (board_status_array[piece_coordinate.y][piece_coordinate.x] == "space") {
+        let now_num = number_Calculation(now_put_piece_number);
+
+        board_status_array[piece_coordinate.y][piece_coordinate.x] = "" + now_num;
+        search_enemy_osero_rule(now_num, piece_coordinate);
+        search_enemy_go_rule(now_num, piece_coordinate);
+
+        if (can_put_position_osero_rule.length) {
+            take_piece_osero_rule(now_num, piece_coordinate);
+        };
+
+        if (can_take_piece_go_rule_array.length) {
+            take_piece_go_rule();
+        };
+
         if (now_put_piece_number == DECK) {
             for (let y = 0; y < BOARD_SIZE; ++y) {
                 for (let x = 0; x < BOARD_SIZE; ++x) {
@@ -79,30 +93,35 @@ let clicked_board = (e) => {
                     };
                 };
             };
-        };
-        let now_num;
 
-        if (now_put_piece_number % 5) {
-            now_num = now_put_piece_number % 5;
-        } else {
-            now_num = 5;
-        };
-        board_status_array[piece_coordinate.y][piece_coordinate.x] = "" + now_num;
-        search_enemy_osero_rule(now_num, piece_coordinate);
-        search_enemy_go_rule(now_num, piece_coordinate);
+            if (!check_next_turn(number_Calculation(now_put_piece_number + 1))) {
+                if (is_black_turn) {
+                    alert(`後手が${number_Calculation(now_put_piece_number + 1)}を持っていません`);
+                    alert("先手の勝ちです");
+                    gameend_process("先手");
+                    insertion_array_data_to_board();
+                    insertion_array_data_to_piece_table();
 
-        if (can_put_position_osero_rule.length) {
-            take_piece_osero_rule(now_num, piece_coordinate);
-        };
+                    document.getElementById("next_game_button").style.display = "block"
+                    return;
+                } else {
+                    alert(`先手が${number_Calculation(now_put_piece_number + 1)}を持っていません`);
+                    alert("後手の勝ちです");
+                    gameend_process("後手");
+                    insertion_array_data_to_board();
+                    insertion_array_data_to_piece_table();
 
-        if (can_take_piece_go_rule_array.length) {
-            take_piece_go_rule();
+                    document.getElementById("next_game_button").style.display = "block"
+                    return;
+                };
+            };
         };
 
         ++now_put_piece_number;
         is_black_turn = !is_black_turn;
         insertion_array_data_to_board();
         insertion_array_data_to_piece_table();
+        change_turn_display();
     };
 };
 
@@ -114,13 +133,7 @@ let clicked_board_no_deck = (e) => {
     };
 
     if (board_status_array[piece_coordinate.y][piece_coordinate.x] == "space" && selected_took_piece) {
-        let now_num;
-
-        if (now_put_piece_number % 5) {
-            now_num = now_put_piece_number % 5;
-        } else {
-            now_num = 5;
-        };
+        let now_num = number_Calculation(now_put_piece_number);
 
         if (select_took_piece_data.number == now_num) {
             if (is_black_turn && select_took_piece_data.owner == "black") {
@@ -130,7 +143,7 @@ let clicked_board_no_deck = (e) => {
             } else {
                 alert("それは敵の駒です");
                 selected_took_piece = false;
-                paint_color(`${select_took_piece_data.number}_${select_took_piece_data.owner}_storage`, false);
+                document.getElementsByClassName("selected")[0].classList.remove("selected");
                 return;
             };
 
@@ -146,25 +159,41 @@ let clicked_board_no_deck = (e) => {
                 take_piece_go_rule();
             };
 
+            if (!check_next_turn(number_Calculation(now_put_piece_number + 1))) {
+                if (is_black_turn) {
+                    alert(`後手が${number_Calculation(now_put_piece_number + 1)}を持っていません`);
+                    alert("先手の勝ちです");
+                    gameend_process("先手");
+                    insertion_array_data_to_board();
+                    insertion_array_data_to_piece_table();
+
+                    document.getElementsByClassName("selected")[0].classList.remove("selected");
+                    document.getElementById("next_game_button").style.display = "block"
+                    return;
+                } else {
+                    alert(`先手が${number_Calculation(now_put_piece_number + 1)}を持っていません`);
+                    alert("後手の勝ちです");
+                    gameend_process("後手");
+                    insertion_array_data_to_board();
+                    insertion_array_data_to_piece_table();
+
+                    document.getElementsByClassName("selected")[0].classList.remove("selected");
+                    document.getElementById("next_game_button").style.display = "block"
+                    return;
+                };
+            };
+
             ++now_put_piece_number;
             is_black_turn = !is_black_turn;
             selected_took_piece = false;
+            document.getElementsByClassName("selected")[0].classList.remove("selected");
             insertion_array_data_to_board();
             insertion_array_data_to_piece_table();
-
-            if (!check_next_turn(now_num + 1)) {
-                if (is_black_turn) {
-                    alert(`後手が${now_num + 1}を持っていません`);
-                    alert("先手の勝ちです");
-                } else {
-                    alert(`先手が${now_num + 1}を持っていません`);
-                    alert("後手の勝ちです");
-                };
-            };
+            change_turn_display();
         } else {
             alert(`今は${now_num}を置くターンです`);
             selected_took_piece = false;
-            paint_color(`${select_took_piece_data.number}_${select_took_piece_data.owner}_storage`, false)
+            document.getElementsByClassName("selected")[0].classList.remove("selected");
         };
     };
 };
@@ -177,9 +206,9 @@ let select_piece_process = (e) => {
     };
 
     if (selected_took_piece) {
-        paint_color(piece_id, false);
+        document.getElementsByClassName("selected")[0].classList.remove("selected");
     } else {
-        paint_color(piece_id, true);
+        document.getElementById(piece_id).classList.add("selected");
     }
     selected_took_piece = !selected_took_piece;
 };
@@ -229,7 +258,7 @@ function object_Calculation(piece_coordinate, direction) {
     return temp;
 };
 
-function can_take_piece_go_rule(now_num, piece_coordinate, searched_position) {
+function can_take_piece_go_rule(now_num, piece_coordinate, searched_position, searching_group) {
     for (let i = 0; i < direction.length; ++i) {
         let position = object_Calculation(piece_coordinate, direction[direction[i]]);
         let array_position = board_status_array[position.y][position.x];
@@ -239,26 +268,27 @@ function can_take_piece_go_rule(now_num, piece_coordinate, searched_position) {
             let piece_id = position.y + "_" + position.x;
             if (!searched_position.includes(piece_id)) {
                 searched_position.push(piece_id);
-                if (can_take_piece_go_rule(now_num, position, searched_position)) {
+                if (can_take_piece_go_rule(now_num, position, searched_position, searching_group)) {
                     return true;
                 };
             };
         };
     };
-    can_take_piece_go_rule_array.push(piece_coordinate);
+    searching_group.push(piece_coordinate);
     return false;
 };
 
 function search_enemy_go_rule(now_num, piece_coordinate) {
-    let searched_position = [];
     for (let i = 0; i < direction.length; ++i) {
         let position = object_Calculation(piece_coordinate, direction[direction[i]]);
         let array_position = board_status_array[position.y][position.x];
+        let searching_group = [];
+        let searched_position = [];
         if (array_position != "space" && array_position != "outzone" && array_position != now_num) {
             searched_position.push(position.y + "_" + position.x)
-            if (can_take_piece_go_rule(now_num, position, searched_position)) {
-                can_take_piece_go_rule_array = [];
-            };
+            if (!can_take_piece_go_rule(now_num, position, searched_position, searching_group)) {
+                Array.prototype.push.apply(can_take_piece_go_rule_array, searching_group);
+            }
         };
     };
 };
@@ -324,7 +354,7 @@ function take_piece_go_rule() {
         };
         if (board_status_array[position.y][position.x] != "space") {
             if (is_black_turn) {
-                ++white_pieces[parseInt(board_status_array[position.y][position.x])];
+                ++black_pieces[parseInt(board_status_array[position.y][position.x])];
                 board_status_array[position.y][position.x] = "space";
             } else {
                 ++white_pieces[parseInt(board_status_array[position.y][position.x])];
@@ -332,21 +362,63 @@ function take_piece_go_rule() {
             };
         };
     };
+    can_take_piece_go_rule_array = [];
 };
 
 function check_next_turn(next_num) {
     if (is_black_turn) {
-        return black_pieces[next_num];
-    } else {
         return white_pieces[next_num];
+    } else {
+        return black_pieces[next_num];
     };
 };
 
-function paint_color(id, paint) {
-    if (paint) {
-        document.getElementById(id).style.backgroundColor = "red";
+function change_turn_display() {
+    if (is_black_turn) {
+        document.getElementById("turn").style.color = "black";
+        document.getElementById("turn").style.backgroundColor = "white";
+        document.getElementById("turn").innerHTML = "先手番"
     } else {
-        document.getElementById(id).style.backgroundColor = "rgba(0,0,0,0)"
+        document.getElementById("turn").style.color = "white";
+        document.getElementById("turn").style.backgroundColor = "black";
+        document.getElementById("turn").innerHTML = "後手番"
+    };
+    document.getElementById("title").innerHTML = `ジュンゴ  ${now_put_piece_number}ターン目`;
+};
+
+function number_Calculation(num) {
+    if (num % 5) {
+        return num % 5;
+    } else {
+        return 5;
+    };
+};
+
+function gameend_process(winner) {
+    for (let y = 0; y < BOARD_SIZE; ++y) {
+        for (let x = 0; x < BOARD_SIZE; ++x) {
+            document.getElementById((y + 1) + "_" + (x + 1)).removeEventListener("click", clicked_board_no_deck, false);
+        };
+    };
+
+    for (let y = 0; y < 2; ++y) {
+        for (let x = 0; x < 3; ++x) {
+            if (y * 3 + x + 1 != 6) {
+                document.getElementById(`${y * 3 + x + 1}_black_storage`).removeEventListener("click", select_piece_process, false);
+                document.getElementById(`${y * 3 + x + 1}_white_storage`).removeEventListener("click", select_piece_process, false);
+            };
+        };
+    };
+
+    document.getElementById("turn").style.color = "white";
+    document.getElementById("turn").style.backgroundColor = "#87bdd8";
+    document.getElementById("turn").innerHTML = `勝者:${winner}`;
+};
+
+function next_game_process() {
+    let next_game = confirm("次の試合をしますか？");
+    if (next_game) {
+        location.href = location.href;
     };
 };
 
@@ -406,25 +478,28 @@ function create_piece_table() {
         for (let x = 0; x < 3; ++x) {
             if (y * 3 + x + 1 != 6) {
                 let html_td_black = document.createElement("td");
-                let html_td_white = document.createElement("td");
                 let html_p_black = document.createElement("p");
-                let html_p_white = document.createElement("p");
                 let html_img_black = document.createElement("img");
-                let html_img_white = document.createElement("img");
 
                 html_td_black.id = `${y * 3 + x + 1}_black_storage`;
-                html_td_white.id = `${y * 3 + x + 1}_white_storage`;
-                //html_td.addEventListener("click", took_piece_selecte_process, false);
                 html_img_black.src = `number_${y * 3 + x + 1}.png`;
-                html_img_white.src = `number_${y * 3 + x + 1}.png`;
                 html_p_black.innerHTML = "0";
-                html_p_white.innerHTML = "0";
                 html_td_black.appendChild(html_img_black);
-                html_td_white.appendChild(html_img_white);
-
                 html_td_black.appendChild(html_p_black);
-                html_td_white.appendChild(html_p_white);
                 html_tr_black.appendChild(html_td_black);
+            };
+
+            if (y * 3 + x + 1 != 3) {
+                let html_td_white = document.createElement("td");
+                let html_p_white = document.createElement("p");
+                let html_img_white = document.createElement("img");
+
+                html_td_white.id = `${(4 - y * 3) + x}_white_storage`;
+                html_img_white.src = `number_${(4 - y * 3) + x}.png`;
+                html_img_white.classList.add("enemy");
+                html_p_white.innerHTML = "0";
+                html_td_white.appendChild(html_img_white);
+                html_td_white.appendChild(html_p_white);
                 html_tr_white.appendChild(html_td_white);
             };
         };
@@ -436,3 +511,4 @@ function create_piece_table() {
 create_board_array();
 create_board();
 create_piece_table();
+document.getElementById("next_game_button").addEventListener("click", next_game_process, false);
